201d1c99ee172e2e10622b3d7072f36482b3b6b0
penguinlion elementary os <qetup1988@gmail.com>
2021-03-12 01:39:18 +0800

    update 10023

--- 10023(Not AC)/10023.py
+++ 10023(Not AC)/10023.py
@@ -1,21 +1,16 @@
-def isAlmostEqualSquare(guess, bigNum):
+def isEqualSquare(guess, bigNum):
 	guessSq = guess*guess
 	if guessSq == bigNum:
 		return True
-#	elif guessSq<bigNum and (guess+1)*(guess+1)>bigNum:
-#		return True
 	return False
 
 def mySqrt(bigNum):
 	length = len(bigNum)
 	guessBig = bigNum = int(bigNum)
 	guessSmall = int(0)
-	if(length>10):
-		guessBig = int('1'*(length//7*4))
-		guessSmall = int('1'*(length//7*3))
 	guess = (guessBig+guessSmall)//2
 	guessSq = guess*guess
-	while not isAlmostEqualSquare(guess, bigNum):
+	while not isEqualSquare(guess, bigNum):
 		if guessSq > bigNum:
 			guessBig = guess-1
 		else:
