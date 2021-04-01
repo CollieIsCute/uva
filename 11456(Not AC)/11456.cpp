c08a5d6d6b835aea9fadbfa0832bf8ce3d432c4b
penguinlion elementary os <qetup1988@gmail.com>
2021-04-01 14:28:56 +0800

    finish 725

--- 11456(Not AC)/11456.cpp
+++ 11456(Not AC)/11456.cpp
@@ -1,4 +1,35 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {}
+int LIS( vector< int > const &v ) {
+	vector< int > temp;
+	if ( !( v.size() ) )
+		return 0;
+	temp.push_back( v[ 0 ] );
+	for ( int i = 1; i < v.size(); i++ ) {
+		if ( v[ i ] < temp.back() ) {
+			temp.pop_back();
+			temp.push_back( v[ i ] );
+		}
+		else if ( v[ i ] > temp.back() ) {
+			temp.push_back( v[ i ] );
+		}
+	}
+	return temp.size();
+}
+
+int main() {
+	int loopCase, numOfTrain;
+	int temp;
+	vector<int> train;
+	cin >> loopCase;
+	for ( int i = 0; i < loopCase; i++ ) {
+		cin >> numOfTrain;
+		for ( int j = 0; j < numOfTrain; j++ ) {
+			cin>>temp;
+			train.push_back(temp);
+		}
+		cout<<LIS(train);
+	}
+}
