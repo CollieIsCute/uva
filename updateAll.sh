a250261adb4517727ff6ccda27e65a7c123f6b60
penguinlion <qetup1988@gmail.com>
2021-05-14 17:23:57 +0800

    add "missing flie" message shown to terminal

--- updateAll.sh
+++ updateAll.sh
@@ -25,32 +25,43 @@ head -n $temp $file > new_README.md
 rm $file
 mv new_README.md $file
 #update ReadmeTable
-echo -e "- 共<b> "`ls | sort | grep '[0-9]' | wc -w`" </b>題\n" >> $file
+echo -e "- 共<b> "`ls | sort -n | grep '[0-9]' | wc -w`" </b>題\n" >> $file
 echo '| 題號 | 星數 | source code | screenshot | makefile | solution.txt |' >> $file
 echo '| :--------: | :--------: | :--------: | :--------: | :--------: | :--------: |' >> $file
-for problemNum in $(ls | sort | grep '[0-9]')
+lack=()
+for problemNum in $(ls | sort -n | grep '[0-9]')
 do
 	echo -n "| <b>$problemNum</b> |" >> $file
 	printLevel $problemNum
 	if [ -e $problemNum\/$problemNum.* ]; then
 		echo -n ' O |' >> $file
 	else
-		echo -n ' X |' >> $file
+		echo -n ' <b>X</b> |' >> $file
+		lack+=("$problemNum source code")
 	fi
 	if [ -e $problemNum\/*Screenshot* ]; then
 		echo -n ' O |' >> $file
 	else
-		echo -n ' X |' >> $file
+		echo -n ' <b>X</b> |' >> $file
+		lack+=("$problemNum screenshot")
 	fi
 	if [ -e $problemNum\/[Mm]akefile ]; then
 		echo -n ' O |' >> $file
 	else
-		echo -n ' X |' >> $file
+		echo -n ' <b>X</b> |' >> $file
+		lack+=("$problemNum makefile")
 	fi
 	if [ -e $problemNum\/[sS]olution.txt ]; then
 		echo -n ' O |' >> $file
 	else
-		echo -n ' X |' >> $file
+		echo -n ' <b>X</b> |' >> $file
+		lack+=("$problemNum solution.txt")
 	fi
 	echo >>$file
+done
+
+# print out which file needed to terminal
+for line in "${lack[@]}"
+do
+	echo updateAll: need $line
 done
