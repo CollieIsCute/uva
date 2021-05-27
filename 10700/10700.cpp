79f8cbc81660f9b6f40d70ca87f336c9ec09f0e0
penguinlion <qetup1988@gmail.com>
2021-05-28 00:45:40 +0800

    add 11259

--- 10700/10700.cpp
+++ 10700/10700.cpp
@@ -1,75 +1,76 @@
 #include <iostream>
-#include <string>
 #include <sstream>
+#include <string>
 #include <vector>
 using namespace std;
 using ll = long long;
 
-ll getMax(istringstream &max){
-	vector<ll> numbers;
-	vector<char> x;
+ll getMax( istringstream& max ) {
+	vector< ll > numbers;
+	vector< char > x;
 	ll num1, num2;
 	char now;
 	max >> num1;
-	while(!max.eof()){
+	while ( !max.eof() ) {
 		max >> now;
 		max >> num2;
-		if(now == '+'){
+		if ( now == '+' ) {
 			num1 = num1 + num2;
 		}
-		else{
-			numbers.push_back(num1);
-			x.push_back(now);
+		else {
+			numbers.push_back( num1 );
+			x.push_back( now );
 			num1 = num2;
 		}
 	}
-	numbers.push_back(num1);
+	numbers.push_back( num1 );
 	ll ans = 1;
-	while(!numbers.empty()){
+	while ( !numbers.empty() ) {
 		ans *= numbers.back();
 		numbers.pop_back();
 	}
 	return ans;
 }
 
-ll getMin(istringstream &min){
-	vector<ll> numbers;
-	vector<char> x;
+ll getMin( istringstream& min ) {
+	vector< ll > numbers;
+	vector< char > x;
 	ll num1, num2;
 	char now;
 	min >> num1;
-	while(!min.eof()){
+	while ( !min.eof() ) {
 		min >> now;
 		min >> num2;
-		if(now == '*'){
+		if ( now == '*' ) {
 			num1 = num1 * num2;
 		}
-		else{
-			numbers.push_back(num1);
-			x.push_back(now);
+		else {
+			numbers.push_back( num1 );
+			x.push_back( now );
 			num1 = num2;
 		}
 	}
-	numbers.push_back(num1);
+	numbers.push_back( num1 );
 	ll ans = 0;
-	while(!numbers.empty()){
+	while ( !numbers.empty() ) {
 		ans += numbers.back();
 		numbers.pop_back();
 	}
 	return ans;
 }
 
-int main(){
+int main() {
 	int n;
-	cin>>n;
+	cin >> n;
 	string line;
 
-	for(int i = 0; i < n; i++){
-		getline(cin, line);
-		if(line == "") getline(cin, line);
-		istringstream maxLine(line), minLine(line);
-		ll big = getMax(maxLine);
-		ll small = getMin(minLine);
-		cout<<"The maximum and minimum are "<<big<<" and "<<small<<"."<<endl;
+	for ( int i = 0; i < n; i++ ) {
+		getline( cin, line );
+		if ( line == "" )
+			getline( cin, line );
+		istringstream maxLine( line ), minLine( line );
+		ll big = getMax( maxLine );
+		ll small = getMin( minLine );
+		cout << "The maximum and minimum are " << big << " and " << small << "." << endl;
 	}
 }
