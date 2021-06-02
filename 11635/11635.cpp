1cb9b8e81f0c08a27bb770dae75e10a44fba1056
penguinlion <qetup1988@gmail.com>
2021-06-03 00:21:41 +0800

    update 11635

--- 11635/11635.cpp
+++ 11635/11635.cpp
@@ -1,95 +1,108 @@
-#include <iostream>
+#include<iostream>
 #include <queue>
-#include <vector>
-
+#include <unordered_set>
 using namespace std;
-const int MAX_N = 10005;
-const int MAX_H = 105;
-const int MAX_TIME = 600;
-const int INFINITE = 7000000;
 
-class Vertex {
+const int MAX_CITIES = 10005;
+const int MAX_HOTELS = 102;
+const int MAX_DIST = 599;
+const int INF = 1000000;
+
+class Vertex{
 public:
-	int number = 0, edgeWeight = 0;
-	Vertex( int v, int w ) : number( v ), edgeWeight( w ) {}
+	unordered_set<int> connections;
 };
 
-vector< Vertex > adjList[ MAX_N ];
-int destination[ MAX_N ];
+void initAdjCities(){
+	for(auto& i: adjCities)
+		for(auto& j: i)
+			j = INF;
+	for(int i = 0; i < MAX_CITIES; i++)
+		adjCities[i][i] = 0;
+}
 
-void SPFA( int source ) {
-	queue< Vertex > Q;	// initialize
-	bool hasInQ[ MAX_N ] = {};
-	for ( int d : destination ) {
-		d = INFINITE;
+void initAdjHotels(){
+	for(auto& i: adjHotels)
+		for(auto& j: i)
+			j = 0;
+	for(int i = 0; i < MAX_HOTELS; i++)
+		adjHotels[i][i] = 1;
+}
+
+void initAllCities(){
+	for(auto& i: allCities)
+		i.connections.clear();
+}
+
+void SPFA(int source){
+	queue<int> vertexForRelax;
+	vertexForRelax.push(source);
+	
+	int distFromSource[MAX_CITIES];
+	for(int i = 0; i < MAX_CITIES; i++){
+		distFromSource[i] = INF;
 	}
-	destination[ source ] = 0;
-	Q.push( Vertex( source, 0 ) );
-	hasInQ[ source ] = true;
-
-	while ( !Q.empty() ) {
-		int now = Q.front().number;
-		Q.pop();
-		hasInQ[ now ] = false;
-		for ( Vertex toNeighbor : adjList[ now ] ) {
-			int value = destination[ now ] + toNeighbor.edgeWeight;
-			if ( value < destination[ toNeighbor.number ] ) {
-				destination[ toNeighbor.number ] = value;
-				if ( !hasInQ[ toNeighbor.number ] )
-					Q.push( Vertex( toNeighbor.number, toNeighbor.edgeWeight ) );
-			}
+	while(!vertexForRelax.empty()){
+		relax(vertexForRelax);
+	}
+	for(int i = 0; i < MAX_HOTELS; i++){
+		if(distFromSource[hotels[i]]<MAX_DIST){
+			adjHotels[hotels[i]][source] = 1;
+			adjHotels[source][hotels[i]] = 1;
 		}
 	}
 }
 
-int main() {
-	int vertexNum, inputH, edgeNum, hotels[ MAX_N ];
-	int hotelAdj[ MAX_H ][ MAX_H ];
-	while ( cin >> vertexNum && vertexNum ) {
-		cin >> inputH;
-		hotels[ 1 ] = 1;
-		hotels[ 2 ] = inputH;
-		int hotelNum = 2;  // add source and end
-		int thisHotel;
-		for ( int i = 0; i < inputH; i++ ) {
-			cin >> thisHotel;
-			hotels[ ++hotelNum ] = thisHotel;
+void relax(queue<int>& vertexForRelax){
+	int now;
+	now = vertexForRelax.front();
+	vertexForRelax.pop();
+	for(int& n :allCities[now].connections){
+		int newDistance = distFromSource[now] + adjCities[now][n];
+		if(newDistance < distFromSource[n]){
+			distFromSource[n] = newDistance;
+			vertexForRelax.push(n);
 		}
-		cin >> edgeNum;
-		int a, b, w;
-		for ( int i = 0; i < edgeNum; i++ ) {
-			cin >> a >> b >> w;
-			adjList[ a ].push_back( Vertex( b, w ) );
-			adjList[ b ].push_back( Vertex( a, w ) );
-		}
-		for ( int i = 0; i < MAX_H; i++ )
-			for ( int j = 0; j < MAX_H; j++ )
-				hotelAdj[ i ][ j ] = INFINITE;
+	}
+}
 
-		for ( int i = 1; i <= hotelNum; i++ ) {
-			hotelAdj[ i ][ i ] = 0;
-			SPFA( hotels[ i ] );
-			for ( int j = 1; j <= hotelNum; j++ )
-				if ( i != j && destination[ hotels[ j ] ] <= MAX_TIME ) {
-					// cout<<destination[hotels[j]]<<endl;
-					hotelAdj[ i ][ j ] = 1;
-				}
-		}
-		for ( int k = 1; k <= hotelNum; k++ )
-			for ( int i = 1; i <= hotelNum; i++ )
-				for ( int j = 1; j <= hotelNum; j++ )
-					if ( hotelAdj[ i ][ k ] + hotelAdj[ k ][ j ] < hotelAdj[ i ][ j ] )
-						hotelAdj[ i ][ j ] = hotelAdj[ i ][ k ] + hotelAdj[ k ][ j ];
+void setRoad(int pa, int pb, int distance){
+	allCities[pa].connections.insert(pb);
+	allCities[pb].connections.insert(pa);
+	adjCities[pa][pb] = min(distance, adjCities[pa][pb]);
+	adjCities[pb][pa] = min(distance, adjCities[pa][pb]);
+}
 
-		if ( hotelAdj[ 1 ][ 2 ] != INFINITE )
-			cout << hotelAdj[ 1 ][ 2 ] - 1 << endl;
-		else
-			cout << -1 << endl;
-		for ( int i = 0; i < MAX_N; i++ ) {
-			adjList[ i ].clear();
+int BFS(){
+	queue<int> waitForSearchHotels;
+	waitForSearchHotels.push(1);
+}
+
+int adjCities[MAX_CITIES][MAX_CITIES];
+int adjHotels[MAX_HOTELS][MAX_HOTELS];
+Vertex allCities[MAX_CITIES];
+int hotels[MAX_HOTELS];
+
+int main(){
+	int citiesNum, hotelsNum, roadsNum;
+	while(cin>>citiesNum && citiesNum){
+		initAdjCities();
+		initAdjHotels();
+		initAllCities();
+		cin>>hotelsNum;
+		for(int i =0; i < hotelsNum; i++)
+			cin>>hotels[i];
+		hotels[hotelsNum] = 1;			//add first and destination city as hotels in order to do SPFA
+		hotels[hotelsNum+1] = citiesNum;
+		hotelsNum += 2;
+		cin>>roadsNum;
+		int pa, pb, dist;
+		for(int i = 0; i < roadsNum; i++){
+			cin>>pa>>pb>>dist;
+			setRoad(pa, pb, dist);
 		}
-		for ( int i = 0; i < MAX_H; i++ ) {
-			hotels[ i ] = 0;
-		}
+		for(int& i: hotels)
+			SPFA(i);
+		//TODO: BFS or something to get answer
 	}
 }
