6b997cfb23d79267d4e9b7c47f2cff81c86583ee
penguinlion <qetup1988@gmail.com>
2021-06-03 21:28:35 +0800

    ac 11424, add solution, and add screenshot

--- 11424/11424.cpp
+++ 11424/11424.cpp
@@ -2,21 +2,36 @@
 #include <iostream>
 using namespace std;
 using ll = long long;
-const int MAX_N = 20001;
+const int MAX_N = 200001;
+
+inline int getPhi( int n ) {
+	int ans = n;
+	for ( int i = 2; i * i <= n; ++i )
+		if ( n % i == 0 ) {
+			ans = ans / i * ( i - 1 );
+			while ( n % i == 0 )
+				n /= i;
+		}
+	if ( n > 1 )
+		ans = ans / n * ( n - 1 );
+	return ans;
+}
 
 int main() {
 	int N;
-	ll sumOfGCD[ MAX_N ] = {}, ans;
-	for ( int i = 2; i < MAX_N; i++ ) {
-		for ( int j = 1; j < i; j++ ) {
-			sumOfGCD[ i ] += __gcd( i, j );
-		}
-		cout << sumOfGCD[ i ] << ",";
-	}
-	while ( cin >> N && N ) {
-		ans = 0;
-		for ( int i = 2; i <= N; i++ )
-			ans += sumOfGCD[ i ];
-		cout << ans << endl;
-	}
+	ll phi[ MAX_N ], sumOfGCD[ MAX_N ], ans[ MAX_N ];
+
+	for ( int i = 1; i < MAX_N; i++ )
+		phi[ i ] = getPhi( i );
+
+	for ( int i = 2; i < MAX_N; i++ )
+		for ( int j = 1; i * j < MAX_N; j++ )
+			sumOfGCD[ i * j ] += phi[ i ] * j;
+
+	ans[ 0 ] = ans[ 1 ] = 0;
+	for ( int i = 2; i < MAX_N; i++ )
+		ans[ i ] = ans[ i - 1 ] + sumOfGCD[ i ];
+
+	while ( cin >> N && N )
+		cout << ans[ N ] << endl;
 }
