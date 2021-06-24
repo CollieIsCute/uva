e8b9cee631a9388c0736ae46ca83b647a08cf6a2
-d <qetup1988@gmail.com>
2021-06-24 22:30:24 +0800

    update 11635

--- 11635/11635.cpp
+++ 11635/11635.cpp
@@ -13,70 +13,13 @@ public:
 	unordered_set< int > connections;
 };
 
-void initAdjCities() {
-	for( auto& i : adjCities )
-		for( auto& j : i )
-			j = INF;
-	for( int i = 0; i < MAX_CITIES; i++ )
-		adjCities[ i ][ i ] = 0;
-}
-
-void initAdjHotels() {
-	for( auto& i : adjHotels )
-		for( auto& j : i )
-			j = 0;
-	for( int i = 0; i < MAX_HOTELS; i++ )
-		adjHotels[ i ][ i ] = 1;
-}
-
-void initAllCities() {
-	for( auto& i : allCities )
-		i.connections.clear();
-}
-
-void SPFA( int source ) {
-	queue< int > vertexForRelax;
-	vertexForRelax.push( source );
-
-	int distFromSource[ MAX_CITIES ];
-	for( int i = 0; i < MAX_CITIES; i++ ) {
-		distFromSource[ i ] = INF;
-	}
-	while( !vertexForRelax.empty() ) {
-		relax( vertexForRelax );
-	}
-	for( int i = 0; i < MAX_HOTELS; i++ ) {
-		if( distFromSource[ hotels[ i ] ] < MAX_DIST ) {
-			adjHotels[ hotels[ i ] ][ source ] = 1;
-			adjHotels[ source ][ hotels[ i ] ] = 1;
-		}
-	}
-}
-
-void relax( queue< int >& vertexForRelax ) {
-	int now;
-	now = vertexForRelax.front();
-	vertexForRelax.pop();
-	for( int& n : allCities[ now ].connections ) {
-		int newDistance = distFromSource[ now ] + adjCities[ now ][ n ];
-		if( newDistance < distFromSource[ n ] ) {
-			distFromSource[ n ] = newDistance;
-			vertexForRelax.push( n );
-		}
-	}
-}
-
-void setRoad( int pa, int pb, int distance ) {
-	allCities[ pa ].connections.insert( pb );
-	allCities[ pb ].connections.insert( pa );
-	adjCities[ pa ][ pb ] = min( distance, adjCities[ pa ][ pb ] );
-	adjCities[ pb ][ pa ] = min( distance, adjCities[ pa ][ pb ] );
-}
-
-int BFS() {
-	queue< int > waitForSearchHotels;
-	waitForSearchHotels.push( 1 );
-}
+void initAdjCities();
+void initAdjHotels();
+void initAllCities();
+void SPFA( int hotelIndex );
+void relax( queue< int >& vertexForRelax, int* distFromSource );
+void setRoad( int pa, int pb, int distance );
+int BFS();
 
 int adjCities[ MAX_CITIES ][ MAX_CITIES ];
 int adjHotels[ MAX_HOTELS ][ MAX_HOTELS ];
@@ -101,8 +44,78 @@ int main() {
 			cin >> pa >> pb >> dist;
 			setRoad( pa, pb, dist );
 		}
-		for( int& i : hotels )
+		for( int i = 0; i < hotels.size(); i++ )
 			SPFA( i );
 		// TODO: BFS or something to get answer
+	}
+}
+
+void initAdjCities() {
+	for( auto& i : adjCities )
+		for( auto& j : i )
+			j = INF;
+	for( int i = 0; i < MAX_CITIES; i++ )
+		adjCities[ i ][ i ] = 0;
+}
+
+void initAdjHotels() {
+	for( auto& i : adjHotels )
+		for( auto& j : i )
+			j = 0;
+	for( int i = 0; i < MAX_HOTELS; i++ )
+		adjHotels[ i ][ i ] = 1;
+}
+
+void initAllCities() {
+	for( auto& i : allCities )
+		i.connections.clear();
+}
+
+void SPFA( int hotelIndex ) {
+	queue< int > vertexForRelax;
+	vertexForRelax.push( hotels[hotelIndex] );
+
+	int distFromSource[ MAX_CITIES ];
+	for( int i = 0; i < MAX_CITIES; i++ ) { //initialize
+		distFromSource[ i ] = INF;
+	}
+	distFromSource[ hotels[ hotelIndex ] ] = 0;
+	
+	while( !vertexForRelax.empty() ) {
+		relax( vertexForRelax, distFromSource );
+	}
+	for( int i = 0; i < MAX_HOTELS; i++ ) {
+		if( distFromSource[ hotels[ i ] ] < MAX_DIST ) {
+			adjHotels[ hotels[ i ] ][ hotelIndex ] = 1;
+			adjHotels[ hotelIndex ][ hotels[ i ] ] = 1;
+		}
+	}
+}
+
+void relax( queue< int >& vertexForRelax, int* distFromSource ) {
+	int now;
+	now = vertexForRelax.front();
+	vertexForRelax.pop();
+	for(const int& n : allCities[ now ].connections ) {
+		int newDistance = distFromSource[ now ] + adjCities[ now ][ n ];
+		if( newDistance < distFromSource[ n ] ) {
+			distFromSource[ n ] = newDistance;
+			vertexForRelax.push( n );
+		}
+	}
+}
+
+void setRoad( int pa, int pb, int distance ) {
+	allCities[ pa ].connections.insert( pb );
+	allCities[ pb ].connections.insert( pa );
+	adjCities[ pa ][ pb ] = distance;
+	adjCities[ pb ][ pa ] = distance;
+}
+
+int BFS() {
+	queue< int > waitForSearchHotels;
+	waitForSearchHotels.push( 1 );
+	while(!waitForSearchHotels.empty()){
+		for(auto i : hotels[])
 	}
 }
